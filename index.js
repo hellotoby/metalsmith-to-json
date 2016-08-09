6f4011e408f611b30bce2b67d1f82281e3177c38
Toby Vervaart <hello@tobyvervaart.com.au>
2016-08-09 12:36:29 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const _ = require('lodash');
+
+module.exports = function( options ) {
+
+    let outputPath      = options.outputPath || null,           // Specify a path (string) to put the json files eg. /api
+        createIndexes   = options.createIndexes || false,       // Specify whether or not to create indexes of the accumulated files
+        indexPaths      = options.indexPaths || false;          // Specify paths to source folders with content to index
+
+    return function( files, metalsmith, done ) {
+
+        _(files).forEach( function( file, key ) {
+
+            let data            = _.omit(file, ['mode', 'stats']);
+            data.contents       = file.contents.toString();
+            data.contents       = new Buffer( JSON.stringify( data ), 'utf8' );
+
+            let filepath        = key.replace('.html', '.json');
+            files[filepath]     = data;
+
+            delete files[key];
+
+        });
+
+        if( creatIndexes ) {
+
+            if( ! indexPaths ) {
+
+                throw new Error('createIndexes is true, but no indexPaths are specified.');
+
+            }
+
+            _(indexPaths).forEach( function( value ) {
+
+                let output      = {},
+                    regex       = new RegExp( value ),
+                    filepath    = value + '.json';
+
+                output.contents = [];
+
+                _(files).forEach(function( file, key ) {
+
+                    if( regex.test( key ) ) {
+                        output.contents.push( file.contents );
+                    };
+
+                });
+
+                output.contents = new Buffer('[' + articles.contents.toString() + ']', 'utf8');
+
+                files[filepath] = output;
+
+            });
+
+        }
+
+        done();
+
+    }
+
+}
